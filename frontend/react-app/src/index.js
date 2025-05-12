315eefde210f6df56c94f318aa427215533da781
Parth <ppmistry204@gmail.com>
2025-05-12 13:05:35 +0530

    Added sentry on cra frontend.

--- frontend/react-app/src/index.js
+++ frontend/react-app/src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as Sentry from "@sentry/react";
+
+const dsn = process.env.REACT_APP_SENTRY_DSN_URL || null;
+if (dsn) {
+  Sentry.init({
+    dsn,
+    // Setting this option to true will send default PII data to Sentry.
+    // For example, automatic IP address collection on events
+    sendDefaultPii: true
+  });
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
